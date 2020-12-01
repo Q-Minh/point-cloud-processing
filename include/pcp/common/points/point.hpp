009d2b332e1783f022dfdcadb9c09d2a2895cba1
Quoc-Minh <tonthat.quocminh@gmail.com>
2020-11-30 23:32:08 -0500

    Add noexcept specifiers for point types

--- include/pcp/common/points/point.hpp
+++ include/pcp/common/points/point.hpp
@@ -25,22 +25,22 @@ class basic_point_t
     void y(T const& value) { y_ = value; }
     void z(T const& value) { z_ = value; }
 
-    basic_point_t()                           = default;
-    basic_point_t(self_type const& other)     = default;
-    basic_point_t(self_type&& other) noexcept = default;
-    self_type& operator=(self_type const& other) = default;
+    basic_point_t() noexcept                       = default;
+    basic_point_t(self_type const& other) noexcept = default;
+    basic_point_t(self_type&& other) noexcept      = default;
+    self_type& operator=(self_type const& other) noexcept = default;
     self_type& operator=(self_type&& other) noexcept = default;
-    basic_point_t(T x, T y, T z) : x_(x), y_(y), z_(z) {}
+    basic_point_t(T x, T y, T z) noexcept : x_(x), y_(y), z_(z) {}
 
     template <class PointView>
-    basic_point_t(PointView const& other) : x_(other.x()), y_(other.y()), z_(other.z())
+    basic_point_t(PointView const& other) noexcept(traits::is_point_v<PointView>) : x_(other.x()), y_(other.y()), z_(other.z())
     {
         static_assert(
             traits::is_point_view_v<PointView>,
             "PointView must satisfy PointView concept");
     }
 
-    friend self_type operator*(coordinate_type k, self_type const& p)
+    friend self_type operator*(coordinate_type k, self_type const& p) noexcept
     {
         return basic_point_t{
             k * p.x_,
@@ -49,13 +49,13 @@ class basic_point_t
         };
     }
 
-    friend self_type operator/(self_type const& p, coordinate_type k)
+    friend self_type operator/(self_type const& p, coordinate_type k) noexcept
     {
         return self_type{p.x_ / k, p.y_ / k, p.z_ / k};
     }
 
     template <class PointView>
-    self_type operator+(PointView const& other) const
+    self_type operator+(PointView const& other) const noexcept(traits::is_point_v<PointView>)
     {
         static_assert(
             traits::is_point_view_v<PointView>,
@@ -64,7 +64,7 @@ class basic_point_t
     }
 
     template <class PointView>
-    self_type operator-(PointView const& other) const
+    self_type operator-(PointView const& other) const noexcept(traits::is_point_v<PointView>)
     {
         static_assert(
             traits::is_point_view_v<PointView>,
@@ -72,7 +72,7 @@ class basic_point_t
         return self_type{x() - other.x(), y() - other.y(), z() - other.z()};
     }
 
-    self_type operator-() const { return self_type{-x(), -y(), -z()}; }
+    self_type operator-() const noexcept { return self_type{-x(), -y(), -z()}; }
 
   private:
     coordinate_type x_ = 0., y_ = 0., z_ = 0.;
--- include/pcp/common/points/point_view.hpp
+++ include/pcp/common/points/point_view.hpp
@@ -26,18 +26,18 @@ class basic_point_view_t
     using coordinate_type = typename point_type::coordinate_type;
     using T               = component_type;
 
-    explicit basic_point_view_t(point_type* point) : point_(point) {}
+    explicit basic_point_view_t(point_type* point) noexcept : point_(point) {}
     /**
      * @brief
      * Default constructing a basic_point_view_t is possible,
      * but beware of using it before having set its underlying
      * pointed-to point.
      */
-    basic_point_view_t()                       = default;
-    basic_point_view_t(self_type const& other) = default;
-    basic_point_view_t(self_type&& other)      = default;
-    self_type& operator=(self_type const& other) = default;
-    self_type& operator=(self_type&& other) = default;
+    basic_point_view_t() noexcept                       = default;
+    basic_point_view_t(self_type const& other) noexcept = default;
+    basic_point_view_t(self_type&& other) noexcept      = default;
+    self_type& operator=(self_type const& other) noexcept = default;
+    self_type& operator=(self_type&& other) noexcept = default;
 
     template <class TPointView>
     self_type& operator=(TPointView const& other)
@@ -58,9 +58,9 @@ class basic_point_view_t
     void y(T value) { point_->y(value); }
     void z(T value) { point_->z(value); }
 
-    void point(point_type* point) { point_ = point; }
-    point_type const* point() const { return point_; }
-    point_type* point() { return point_; }
+    void point(point_type* point) noexcept { point_ = point; }
+    point_type const* point() const noexcept { return point_; }
+    point_type* point() noexcept { return point_; }
 
   private:
     point_type* point_;
