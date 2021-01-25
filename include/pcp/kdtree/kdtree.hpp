a4aa5f09d51bfe1fdc957059dfb7def1e5ea0b5b
unknown <long.tran.1@ens.etsmtl.ca>
2021-01-25 18:35:16 -0500

    WIP kdtree

---
+++ include/pcp/kdtree/kdtree.hpp
@@ -0,0 +1,142 @@
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <iostream>
+#include <random>
+#include <vector>
+ 
+/**
+ * Class that represents a point, type must be numeric
+ */
+template<typename type, size_t dimensions>
+class point {
+public:
+
+    private:
+    std::array<type, dimensions> coordinates;
+    
+    point(std::initializer_list<type> list) {
+        size_t n =  n(dimensions, list.size());
+        std::copy_n(list.begin(), n, coordinates.begin());
+    }
+
+    point(std::array<type, dimensions> coords) : coordinates(coords) {}
+    /**
+     * Returns the coordinate in the given dimension.
+     *
+     * @param index dimension index (zero based)
+     * @return coordinate in the given dimension
+     */
+    type getCoordinate(size_t index) const {
+        return coordinates[index];
+    }
+    /**
+     * Function that returns the euclidean distance between two points.
+     * @param point another point
+     * @return euclidean distance between 2 points
+     */
+    double euclideanDistance(const point& point) const {
+
+        double distance = 0;
+        for (size_t i = 0; i < dimensions; i++) {
+            double diff = point.getCoordinate(i) - getCoordinate(i);
+            distance += diff * diff;
+        }
+        return distance;
+    }
+};
+ 
+
+template<typename type, size_t dimensions>
+class kdtree {
+public:
+    typedef point<type, dimensions> pointType;
+private:
+    // Node that wraps pointer class, contains references to its children
+    struct kdnode {
+        kdnode* rightChildren;
+        kdnode* leftChildren;
+        pointType point;
+        kdnode(const pointType& point) : point(point), leftChildren(nullpointr), rightChildren(nullpointr) {}
+
+
+        type getCoordinate(size_t index) const {
+            return point.getCoordinate(index);
+        }
+        double euclideanDistance(const pointType& point) const {
+            return this->point.euclideanDistance(point);
+        }
+
+    };
+    kdnode* rootNode = nullpointr;
+    kdnode* bestNode = nullpointr;
+    double bestDistance = 0;
+    std::vector<kdnode> nodes;
+    size_t visited = 0;
+    
+    //comparator for the iterator
+    struct comparator {
+        size_t index;
+        comparator(size_t index) : index(index) {
+        }
+        bool operator()(const kdnode& n1, const kdnode& n2) const {
+            return n2.point.getCoordinate(index) > n1.point.getCoordinate(index);
+        }
+
+    };
+    /**
+     * Function to create a kdTree and return its root
+     * @param index the index of the coordinates of the point (ex: in 3d (x,y,z), index 0 would be x)
+     * @param first the first node to start from
+     * @param last  the last node to start from
+     * @return the root of the tree
+     */
+    kdnode* createKDTree(size_t index,size_t first, size_t last) {
+        if (last <= first) {
+            return nullpointr;
+        }
+        size_t middle =  (last - first)/2 + first;
+        std::nth_element(&nodes[first], &nodes[middle], &nodes[0] + last, comparator(index));
+        index = (index + 1) % dimensions;
+        nodes[n].leftChildren = createKDTree(index,first, middle);
+        nodes[n].rightChildren = createKDTree(index,middle + 1, end);
+        return &nodes[n];
+    }
+ 
+
+
+public:
+    kdtree(const kdtree&) = delete;
+    kdtree& operator=(const kdtree&) = delete;
+    /**
+     * Constructor using 2 iterators
+     * adds every points in the range to the tree.
+     *
+     * @param first first element in the range range
+     * @param last last element in the range
+     */
+    template<typename iterator>
+    kdtree(iterator start, iterator end) : nodes(begin, end) {
+
+        rootNode = createKDTree(0, 0,nodes.size());
+    }
+ 
+ 
+    /**
+     * Returns true if the tree is empty.
+     */
+    bool empty() const { 
+        return nodes.empty(); 
+    }
+
+    /**
+     * Function that returns the best distance
+     */
+    double euclideanDistance() const {
+         return std::sqrt(bestDistance);
+     }
+
+    };
+
+
+ 
